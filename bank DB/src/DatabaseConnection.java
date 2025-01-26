76717bf85b17065678209f0bfd73651eedb3dbb2
Nurbek-dev001 <nur.education001@gmail.com>
2025-01-26 19:48:17 +0500

    Update DatabaseConnection.java

--- bank DB/src/DatabaseConnection.java
+++ bank DB/src/DatabaseConnection.java
@@ -4,8 +4,8 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static final String URL = "jdbc:postgresql://localhost:5432/Bankassig";
-    private static final String USERNAME = "postgres";
-    private static final String PASSWORD = "Karatau+++2024";
+    private static final String USERNAME = "s";
+    private static final String PASSWORD = " ";
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USERNAME, PASSWORD);
     }
